744af8c69416f35192390cef974dc87d4057ca72
nattakarn-collab <nattakarn@macawth.com>
2026-02-19 13:20:24 +0700

    Create config.js

---
+++ web/config.js
@@ -0,0 +1,8 @@
+// === ตั้งค่า 2 อย่างนี้ให้ถูก ===
+window.APP_CONFIG = {
+  LIFF_ID: "2009127023-sgWJern0",                 // <-- LIFF ID ของคุณ
+  APPS_SCRIPT_URL: "PUT_YOUR_APPS_SCRIPT_URL",    // <-- ใส่ URL .../exec ของ Apps Script Web App
+
+  TABS: ["register", "install", "points", "redeem"],
+  DEFAULT_TAB: "register",
+};
