4d69ae5e9e03f6753aa5121d3d6c6d534e42d79f
nattakarn-collab <nattakarn@macawth.com>
2026-02-19 13:24:43 +0700

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 // === ตั้งค่า 2 อย่างนี้ให้ถูก ===
 window.APP_CONFIG = {
   LIFF_ID: "2009127023-sgWJern0",                 // <-- LIFF ID ของคุณ
-  APPS_SCRIPT_URL: "PUT_YOUR_APPS_SCRIPT_URL",    // <-- ใส่ URL .../exec ของ Apps Script Web App
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzyzubdy2RHBv88vZmylzxy2_tL1FzQypLUSmQCbeH9Dq3KOAIKPrkA23DvZln9zx783Q/exec",    // <-- ใส่ URL .../exec ของ Apps Script Web App
 
   TABS: ["register", "install", "points", "redeem"],
   DEFAULT_TAB: "register",
