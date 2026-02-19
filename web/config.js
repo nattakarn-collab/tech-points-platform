0133f856912d4a5ed9be77c2c88184768db74cbf
nattakarn-collab <nattakarn@macawth.com>
2026-02-19 15:10:04 +0700

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,5 +1,7 @@
 // config.js
 window.APP_CONFIG = {
   LIFF_ID: "2009127023-sgWJern0", // <— ใส่ LIFF ID ของคุณ
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzyzubdy2RHBv88vZmylzxy2_tL1FzQypLUSmQCbeH9Dq3KOAIKPrkA23DvZln9zx783Q/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzyzubdy2RHBv88vZmylzxy2_tL1FzQypLUSmQCbeH9Dq3KOAIKPrkA23DvZln9zx783Q/exec",
+  TABS: ["register", "install", "points", "redeem"],
+  DEFAULT_TAB: "register",
 };
