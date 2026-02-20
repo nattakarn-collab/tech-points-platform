0e70dfbf5a85bdb066516f64954cf938bda9fdbb
nattakarn-collab <nattakarn@macawth.com>
2026-02-20 14:48:57 +0700

    Delete docs directory

--- docs/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  LIFF_ID: "2009127023-sgWJern0",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzyzubdy2RHBv88vZmylzxy2_tL1FzQypLUSmQCbeH9Dq3KOAIKPrkA23DvZln9zx783Q/exec",
-  TABS: ["register", "install", "points", "redeem"],
-  DEFAULT_TAB: "register",
-};
